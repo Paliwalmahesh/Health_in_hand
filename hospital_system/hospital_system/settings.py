2f1b4b0e1489ce038d5467bfbf4c2d7e431115e3
Mahesh Paliwal <68917601+Paliwalmahesh@users.noreply.github.com>
2022-08-14 21:52:42 +0530

    SECRET_KEY hided

--- hospital_system/hospital_system/settings.py
+++ hospital_system/hospital_system/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fi!l1*)t==&it0+#j$658942vs3ln&=rm*1r4bwy8-9^%ytxvk'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
